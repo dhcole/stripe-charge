22405938df40ad0c591b098ffa0a813cda7096f3
Dave Cole <dave@developmentseed.org>
2014-11-10 00:06:56 -0500

    Add error handling

--- app.js
+++ app.js
@@ -11,45 +11,65 @@ app.get('/api/health', function(req, res) {
 });
 
 app.post('/api/charge', function(req, res) {
-    var b = req.body,
-        token = b.stripeToken,
-        metaData = {
-          fname: b.fname,
-          lname: b.lname,
-          phone: b.phone,
-          email: b.email,
-          address: b.address,
-          city: b.city,
-          state: b.state,
-          zip: b.zip
-        },
-        customer = {
-          description: 'Online customer',
-          card: token,
-          email: b.email,
-          metadata: metaData
-        },
-        charge = {
-          amount: b.amount * 100,
-          currency: 'usd',
-          description: 'Online charge',
-          metadata: metaData
+  var b = req.body,
+      token = b.stripeToken,
+      metaData = {
+        fname: b.fname,
+        lname: b.lname,
+        phone: b.phone,
+        email: b.email,
+        address: b.address,
+        city: b.city,
+        state: b.state,
+        zip: b.zip
+      },
+      customer = {
+        description: 'Online customer',
+        card: token,
+        email: b.email,
+        metadata: metaData
+      },
+      charge = {
+        amount: b.amount * 100,
+        currency: 'usd',
+        description: 'Online charge',
+        metadata: metaData
 
-        };
+      };
 
     stripe.customers.create(customer, function(err, customer) {
-        if (err) console.warn(err);
+      if (err || !customer || !customer.id) {
+        error(err, res, b);
+        return;
+      }
 
-        if (!customer || !customer.id) res.send(500);
-        charge.customer = customer.id;
+      charge.customer = customer.id;
 
-        stripe.charges.create(charge, function(err, data) {
-            if (err) console.warn(err);
+      stripe.charges.create(charge, function(err, data) {
+        if (err) {
+          error(err, res, b);
+          return;
+        }
 
-            res.redirect(b.success || req.headers['referer']);
-        });
+        res.redirect(b.success || req.headers['referer']);
+      });
     });
 });
+
+function error(err, res, b) {
+  var msg = '',
+      url = '';
+
+  console.warn(err);
+
+  if (b && b.error) {
+    msg = (err && err.message) ? err.message : '';
+    url = b.error + '?error=' + encodeURIComponent(msg);
+    res.redirect(url);
+  } else{
+    res.send(500);
+  }
+}
 
 app.listen(port);
 console.log('Listening on port ' + port);
